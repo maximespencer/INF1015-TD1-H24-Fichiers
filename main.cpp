8cb85775b2b4f5d5160515f95d94ddb8afbdddbc
Maxime_Spencer <maxime.spencer@polymtl.ca>
2024-01-16 13:19:25 -0500

    allo

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int longueurMOT(std::string phrase){
+    std::cout << "Saisissez une phrase : ";
+    std::getline(std::cin, phrase);
+}
