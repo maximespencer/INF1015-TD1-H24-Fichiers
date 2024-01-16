e41385daf47713f8a14afb7e122165342620593d
Maxime_Spencer <maxime.spencer@polymtl.ca>
2024-01-16 14:28:55 -0500

    exo 1 fini

--- main.cpp
+++ main.cpp
@@ -2,8 +2,45 @@
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
+#include <sstream>
 
-int longueurMOT(std::string phrase){
+
+int main(){
+    /*std::string phrase;
     std::cout << "Saisissez une phrase : ";
     std::getline(std::cin, phrase);
+    std::istringstream stream(phrase);
+    std::string mot;
+    std::string motPlusCourt, motPlusLong;
+    size_t longueurMotPlusCourt = std::string::npos;
+    double longueurMotPlusLong,longueurMoyenne,nbMots = 0.0;
+    while(stream>>mot){
+        if (mot.length()>longueurMotPlusLong){
+            motPlusLong = mot;
+            longueurMotPlusLong = mot.length();
+        }
+
+        if (mot.length() < longueurMotPlusCourt) {
+            motPlusCourt = mot;
+            longueurMotPlusCourt = mot.length();
+       
+        }
+        longueurMoyenne += mot.length();
+        nbMots++;
+    }   
+
+    if (nbMots > 0) {
+        longueurMoyenne /= nbMots;
+    }
+    std::cout << "Le mot le plus court est : " << motPlusCourt << std::endl;
+    std::cout << "Le mot le plus long est : " << motPlusLong << std::endl;
+    std::cout << "La longueur moyenne est : " << longueurMoyenne << " lettres" << std::endl;*/
+
+    double U0 = 1;
+    double nombre;
+    double nouvNombre;
+    while((nouvNombre-nombre)){
+        
+    }
+
 }
